1fd263f1af200704204249d387a587a61eb6ba41
Mohd Sayeedul Hoda <sayeedulhoda@gmail.com>
2024-03-09 02:18:16 +0530

    added more json errors handler for more transparency for client

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 	"fmt"
 	"io"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -46,29 +47,67 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any) error{
 
-	err := json.NewDecoder(r.Body).Decode(dst)
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	dec := json.NewDecoder(r.Body)
+
+	// if field is not json but given by client then 
+	// then it will return error if we not insalize the methond then
+	// it will ignore it not return error and it will stop reading json at 
+	// that point
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(dst)
+
 	if err != nil{
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
 		var invalidUnmarshalError *json.InvalidUnmarshalError
+		var maxBytesError *http.MaxBytesError
 
 		switch{
+		// this error happen because
 		case errors.As(err, &syntaxError):
 			return fmt.Errorf("body contain badly-formated JSON (at character %d)", syntaxError.Offset)
+
+		// this error happen due to syntax error in json
 		case errors.Is(err, io.ErrUnexpectedEOF):
 			return errors.New("body contains badly-formated JSON")
+
+		// this error occur when there is json have wrong type of schema that we have define or 
+		// error is related specific field
 		case errors.As(err, &unmarshalTypeError):
 			if unmarshalTypeError.Field != ""{
 				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
 			}
 			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+
+		// this error happen because request body is empty
 		case errors.Is(err, io.EOF):
 			return errors.New("body must not be empty")
+
+		// when client has but extra field that cannot be
+		// map by json decoder then it will return error
+		// starting with "json: unknown"
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field")
+			return	fmt.Errorf("body contains unknown key %s", fieldName)
+
+		case errors.As(err, &maxBytesError):
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytesError.Limit)
+
+		// this error occur when does not pass any thing to json decoder
 		case errors.As(err, &invalidUnmarshalError):
 			panic(err)
+
 		default:
 			return err
 		}
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF{
+		return errors.New("body must only contain a single JSON value")
 	}
 	return nil
 }
