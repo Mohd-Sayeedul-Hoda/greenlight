73839c8a53753632ec6a7dd627ad0872b38595d7
Mohd Sayeedul Hoda <sayeedulhoda@gmail.com>
2024-03-08 15:51:36 +0530

    added errors.go to handle error in json formate

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,6 +10,8 @@ func (app *application) routes() *httprouter.Router{
 	
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
