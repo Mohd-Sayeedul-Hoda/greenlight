fb1ab80bc7dfb1deb9efbb08cf7fb62ab7a6ecf5
Mohd Sayeedul Hoda <sayeedulhoda@gmail.com>
2024-03-09 00:43:13 +0530

    added error handler while parsing the json on post request

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"net/http"
 	"strconv"
+	"fmt"
+	"io"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -39,5 +41,34 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 	w.WriteHeader(status)
 	w.Write(js)
 
+	return nil
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any) error{
+
+	err := json.NewDecoder(r.Body).Decode(dst)
+	if err != nil{
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		switch{
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contain badly-formated JSON (at character %d)", syntaxError.Offset)
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formated JSON")
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != ""{
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+		default:
+			return err
+		}
+	}
 	return nil
 }
