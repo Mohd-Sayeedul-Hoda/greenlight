7575ffcd4c8c8bb8ce46a7e8ba2ecf3e9d24af53
Mohd Sayeedul Hoda <sayeedulhoda@gmail.com>
2024-03-16 05:21:10 +0530

    added to query with page no and page size

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -165,12 +165,14 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 	// @> say if contian pq array 
 	// to_tsvector and plainto_tsquery is changing it title of movies and query
 	// into lexmes meaning "The Batman" into "the" "batman" lower and splitting matching it
-	query := fmt.Sprintf(`SELECT id, created_at, title, year, runtime, genres, version from movies WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '') AND (genres @> $2 OR $2 = '{}') ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
+	query := fmt.Sprintf(`SELECT id, created_at, title, year, runtime, genres, version from movies WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '') AND (genres @> $2 OR $2 = '{}') ORDER BY %s %s, id ASC LIMIT $3 OFFSET $4`, filters.sortColumn(), filters.sortDirection())
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
 	defer cancel()
 
-	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres))
+	args := []any{title, pq.Array(genres), filters.limit(), filters.offset()}
+
+	rows, err := m.DB.QueryContext(ctx, query, args...)
 	if err != nil{
 		return nil, err
 	}
