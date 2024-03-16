8a5df9d5c074d0417850646d3d47a853735f90b0
Mohd Sayeedul Hoda <sayeedulhoda@gmail.com>
2024-03-17 01:09:14 +0530

    added middleware for panic recovery

---
+++ cmd/api/middleware.go
@@ -0,0 +1,23 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+)
+
+func (app *application) recoverPanic(next http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	
+		defer func(){
+			// builtin recover function to check if there has been a panic or not
+			if err := recover(); err != nil{
+				// this will make go http server automatically close
+				w.Header().Set("Connection", "close")
+
+				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
